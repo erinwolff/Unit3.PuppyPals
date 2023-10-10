5ea59fa2f4569e684f69ca21904f585822f8ddb5
Erin Wolff <erin_wolff@outlook.com>
2023-10-10 14:31:21 -0700

    rendered featuredPup to display

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -7,17 +7,27 @@ function App() {
   const [puppies, setPuppies] = useState(puppyList)
   const [featPupId, setFeatPupId] = useState(null)
   console.log(puppies);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup)
+
   return (
     <>
       <div className='puppy-name'>
         {
           puppies.map((puppy) => {
-            const featuredPup= puppies.find((pup) => pup.id === featPupId);
-            console.log(featuredPup);
-            return <p onClick={()=>{setFeatPupId(puppy.id)}}key={puppy.id}>{puppy.name}</p>
-            
-        })
-        }
+            // console.log(featuredPup);
+            return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
+          })
+        }{featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.Age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+
+        )}
       </div>
 
     </>
