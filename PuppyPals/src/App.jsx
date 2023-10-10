7a472182fbe64bd2fabc7e654155f088cb80b1c7
Erin Wolff <erin_wolff@outlook.com>
2023-10-10 12:54:03 -0700

    test commit

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -2,20 +2,18 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import { puppyList } from `./data.js`
 
-// function App() {
-//   const [count, setCount] = useState(0)
 
-//   return (
-//     <>
-      <div>
-    
-      </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
+function App() {
+  
+  return (console.log(puppyList))
+}
+<div></div>
+
+
+
+
+
 
 export default App
