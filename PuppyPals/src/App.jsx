74c98bc6ea22434c7ddcbabb0746ac17c9d259ce
yc530 <yoonachoi530@gmail.com>
2023-10-10 17:01:02 -0400

    added click feature and find for spec pup

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -5,17 +5,20 @@ import { useState } from 'react'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
   console.log(puppies);
   return (
     <>
-      <div>
+      <div className='puppy-name'>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
-          })
+            const featuredPup= puppies.find((pup) => pup.id === featPupId);
+            console.log(featuredPup);
+            return <p onClick={()=>{setFeatPupId(puppy.id)}}key={puppy.id}>{puppy.name}</p>
+            
+        })
         }
       </div>
-
 
     </>
   )
