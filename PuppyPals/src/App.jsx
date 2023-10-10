b0f9f2aad0ed2d82fdfafeb73df316f81b9334c3
Erin Wolff <erin_wolff@outlook.com>
2023-10-10 14:45:09 -0700

    updated styling

--- PuppyPals/src/App.jsx
+++ PuppyPals/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import './index.css'
 import { puppyList } from './data.js'
 import { useState } from 'react'
 
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-  const [featPupId, setFeatPupId] = useState(null)
   console.log(puppies);
+
+  const [featPupId, setFeatPupId] = useState(null)
+
+
   const featuredPup = puppies.find((pup) => pup.id === featPupId);
   console.log(featuredPup)
 
@@ -19,10 +23,10 @@ function App() {
             return <p onClick={() => { setFeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
           })
         }{featPupId && (
-          <div>
+          <div className='featured-pup'>
             <h2>{featuredPup.name}</h2>
             <ul>
-              <li>Age: {featuredPup.Age}</li>
+              <li>Age: {featuredPup.age}</li>
               <li>Email: {featuredPup.email}</li>
             </ul>
           </div>
